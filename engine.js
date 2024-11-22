2c18229dc304ab67443ac5656a8e325d2ad3289f
Évelim Dornelles <dorrrnelles@gmail.com>
2024-11-22 11:31:07 -0300

    upload

--- engine.js
+++ engine.js
@@ -5,3 +5,15 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     menu.classList.toggle('active');
 });
+
+// efeito box imagem //
+
+window.addEventListener('load', () => {
+    const imageBox = document.querySelector('.image-box');
+    const titleBox = document.querySelector('.title-box h1');
+
+    setTimeout(() => {
+        imageBox.classList.add('active');
+        titleBox.classList.add('active');
+    }, 500);
+});
