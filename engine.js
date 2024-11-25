49a45642f48f909459943db4210e0cee345b7e1c
Évelim Dornelles <dorrrnelles@gmail.com>
2024-11-25 12:29:17 -0300

    upload

--- engine.js
+++ engine.js
@@ -6,16 +6,3 @@ hamburger.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
 
-// efeito box imagem
-document.addEventListener('scroll', () => {
-    const titleBox = document.getElementById('title-box');
-    const titleBoxPosition = titleBox.getBoundingClientRect().top;
-    const windowHeight = window.innerHeight;
-
-    // Quando a title-box estiver visível pela primeira vez na tela
-    if (titleBoxPosition <= windowHeight) {
-        titleBox.classList.add('active');
-        // Remove o evento de rolagem após a primeira vez
-        window.removeEventListener('scroll', arguments.callee);
-    }
-});
