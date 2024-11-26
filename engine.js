6dea8b3262b95cc955c34350fbe7a05f584535ee
Évelim Dornelles <dorrrnelles@gmail.com>
2024-11-26 13:36:27 -0300

    upload

--- engine.js
+++ engine.js
@@ -8,11 +8,11 @@ hamburger.addEventListener('click', () => {
 
 window.sr = ScrollReveal({ reset: true });
 
-ScrollReveal().reveal('.title-box', { duration: 2000, rotate: { x: 0, y: 80, z: 0 } });
-ScrollReveal().reveal('.image-box', { duration: 2000 });
-ScrollReveal().reveal('.grid-item', { duration: 2000 });
-ScrollReveal().reveal('.title-container', { duration: 2000 });
-ScrollReveal().reveal('.funcoes-container', { duration: 2000 });
-ScrollReveal().reveal('.grid-item2', { duration: 2000 });
-ScrollReveal().reveal('.vendas-container', { duration: 2000 });
-ScrollReveal().reveal('.contato-container', { duration: 2000, rotate: { x: 0, y: 80, z: 0 } });
+ScrollReveal().reveal('.title-box', { duration: 1000, rotate: { x: 0, y: 80, z: 0 } });
+ScrollReveal().reveal('.image-box', { duration: 1000 });
+ScrollReveal().reveal('.grid-item', { duration: 1000 });
+ScrollReveal().reveal('.title-container', { duration: 1000 });
+ScrollReveal().reveal('.funcoes-container', { duration: 1000 });
+ScrollReveal().reveal('.grid-item2', { duration: 1000 });
+ScrollReveal().reveal('.vendas-container', { duration: 1000 });
+ScrollReveal().reveal('.contato-container', { duration: 1000, rotate: { x: 0, y: 80, z: 0 } });
